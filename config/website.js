09438497ac128714b8ad935441d27e80072c14c3
Adem Hodzic <athos.blade@gmail.com>
2018-11-26 15:34:01 +0100

    Minor tweaks

--- config/website.js
+++ config/website.js
@@ -1,18 +1,18 @@
 const tailwind = require('../tailwind');
 
 module.exports = {
-  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
+  pathPrefix: '/portfolio', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: 'Cara - Gatsby Starter Portfolio', // Navigation and Site Title
-  siteTitleAlt: 'Cara', // Alternative Site title for SEO
-  siteUrl: 'https://portfolio-cara.netlify.com', // Domain of your site. No trailing slash!
+  siteTitle: 'Adem Hodzic Portfolio', // Navigation and Site Title
+  siteTitleAlt: 'Adem', // Alternative Site title for SEO
+  siteUrl: 'https://ademhodzic.github.io/', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
-  siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
+  siteDescription: 'Portfolio page of web developer based in Bosnia and Herzegovina',
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@cara', // Twitter Username
-  ogSiteName: 'cara', // Facebook Site Name
+  userTwitter: '', // Twitter Username
+  ogSiteName: '', // Facebook Site Name
   ogLanguage: 'en_US', // Facebook Language
 
   // Manifest and Progress color
