97e54fbe1bd3e95e66fdfea70ef5463b409c89d7
Hodzic Adem <athos.blade@gmail.com>
2019-02-14 21:08:54 +0100

    changed inital path

--- config/website.js
+++ config/website.js
@@ -1,7 +1,7 @@
 const tailwind = require('../tailwind');
 
 module.exports = {
-  pathPrefix: '/portfolio', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
   siteTitle: 'Adem Hodzic Portfolio', // Navigation and Site Title
   siteTitleAlt: 'Adem', // Alternative Site title for SEO
